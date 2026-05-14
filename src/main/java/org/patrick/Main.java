a04c18f9c02e5d3d2009304e6213485dfa171047
Turtlesaurus731 <patrick.huy.tran@gmail.com>
2026-05-14 19:29:58 -0400

    Prepare Main for deliverable 4 screenshots

--- src/main/java/org/patrick/Main.java
+++ src/main/java/org/patrick/Main.java
@@ -4,32 +4,66 @@ public class Main {
     public static void main(String[] args) {
         Library library = new Library();
 
-        library.addUser(new Student("Patrick"));
-        library.addUser(new Teacher("Potato"));
+        // Users
+        Student student = new Student("Patrick");
+        Teacher teacher = new Teacher("Potato");
         Admin admin = new Admin("Bidoof");
+
+        library.addUser(student);
+        library.addUser(teacher);
         library.addUser(admin);
-        library.addItem(new Book("How to pass my class", "1234567890123", "PatrickA", "Fantasy"));
-        library.addItem(new Book("How to pass my class", "1234567890123", "PatrickA", "Fantasy"));
-        library.addItem(new DVD("How to sleep", "Sleepy Turtle", 123));
-        library.addItem(new DVD("How to sleep", "Sleepy Turtle", 123));
-        library.addItem(new Magazine("Ran out of Ideas", 42, "PatrickPress"));
 
-        library.backupUsersCSV(null);
-        library.backupItemsCSV(null);
-        admin.backupData(library);
-        Library loadedLibrary = new Library();
-        loadedLibrary.loadItemsCSV(null);
-        loadedLibrary.loadUsersCSV(null);
-        System.out.println(loadedLibrary.getItems());
-        System.out.println(loadedLibrary.getUsers());
+        // Items
+        Book book1 = new Book("How to pass my class", "1234567890123", "PatrickA", "Fantasy");
+        DVD dvd1 = new DVD("How to sleep", "Sleepy Turtle", 123);
+        Magazine magazine1 = new Magazine("Ran out of Ideas", 42, "PatrickPress");
 
-        String report = admin.generateReport(library);
-        System.out.println(report);
+        library.addItem(book1);
+        library.addItem(book1);
+        library.addItem(dvd1);
+        library.addItem(dvd1);
+        library.addItem(magazine1);
 
+        // Borrow
+        System.out.println("\nBorrowItemDemonstration");
+        library.borrowItem(student, book1);
+        System.out.println(book1);
+
+        // Return
+        System.out.println("\nReturnItemDemonstration");
+        library.returnItem(student, book1);
+        System.out.println(book1);
+
+        // Stream Search
+        System.out.println("\nStreamSearchDemonstration");
         System.out.println(library.streamSearchByAuthor("PatrickA"));
-        System.out.println(library.searchByCreator("Sleepy Turtle"));
         System.out.println(library.streamSearchByTitle("How to pass my class"));
+        System.out.println(library.searchByCreator("Sleepy Turtle"));
+
+        // Recursive Search
+        System.out.println("\nRecursiveSearchDemonstration");
         System.out.println(library.recursiveSearchByAuthor("PatrickA"));
         System.out.println(library.recursiveSearchByTitle("How to pass my class"));
+
+        // Lost
+        System.out.println("\nLostDemonstration");
+        admin.markItemAsLost(library, magazine1);
+        System.out.println(magazine1);
+
+        // Generate Report
+        System.out.println("\nGenerateReportDemonstration");
+        System.out.println(admin.generateReport(library));
+
+        // CSV backup
+        admin.backupData(library);
+
+        // CSV load
+        System.out.println("\nCSV backup and load Demonstration");
+        Library loadedLibrary = new Library();
+
+        loadedLibrary.loadUsersCSV(null);
+        loadedLibrary.loadItemsCSV(null);
+        System.out.println(loadedLibrary.getUsers());
+        System.out.println(loadedLibrary.getItems());
     }
 }
